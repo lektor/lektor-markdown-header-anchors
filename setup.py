3d11fb19e3ffc903eebe78de36421c9bc3af1026
Armin Ronacher <armin.ronacher@active-4.com>
2015-12-24 23:48:40 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='lektor-markdown-header-anchors',
+    description='Adds support for anchors and table of contents to Markdown.',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    url='http://github.com/lektor/lektor-markdown-header-anchors',
+    version='0.1',
+    license='BSD',
+    py_modules=['lektor_markdown_header_anchors'],
+    entry_points={
+        'lektor.plugins': [
+            'markdown-header-anchors = lektor_markdown_header_anchors:MarkdownHeaderAnchorsPlugin',
+        ]
+    }
+)
