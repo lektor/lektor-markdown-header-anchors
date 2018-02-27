6d086a43aef36838869f11a1ebda1817326f3174
Baruch Sterin <baruchs@gmail.com>
2018-02-26 18:22:10 -0800

    Added tests, three tests should currently fail - will fix later.

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
+tests_require = [
+    'lektor',
+    'mistune',
+    'pytest',
+    'pytest-cov',
+    'pytest-mock'
+]
+
 setup(
     name='lektor-markdown-header-anchors',
     description='Adds support for anchors and table of contents to Markdown.',
@@ -9,6 +17,9 @@ setup(
     version='0.2',
     license='BSD',
     py_modules=['lektor_markdown_header_anchors'],
+    setup_requires=['pytest-runner'],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},    
     entry_points={
         'lektor.plugins': [
             'markdown-header-anchors = lektor_markdown_header_anchors:MarkdownHeaderAnchorsPlugin',
