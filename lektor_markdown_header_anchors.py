46141df527c56debdd86a9ade27a04f7e7f5d334
Folyd <lyshuhow@gmail.com>
2018-11-14 23:56:14 +0800

    Add new anchor-type 'custom' to extract anchor from origin header

--- lektor_markdown_header_anchors.py
+++ lektor_markdown_header_anchors.py
@@ -1,9 +1,10 @@
-from lektor.pluginsystem import Plugin
+import re
 import uuid
-from lektor.utils import slugify
-from markupsafe import Markup
 from collections import namedtuple
 
+from lektor.pluginsystem import Plugin
+from lektor.utils import slugify
+from markupsafe import Markup
 
 TocEntry = namedtuple('TocEntry', ['anchor', 'title', 'children'])
 
@@ -15,12 +16,37 @@ class MarkdownHeaderAnchorsPlugin(Plugin):
     def on_markdown_config(self, config, **extra):
         class HeaderAnchorMixin(object):
             def header(renderer, text, level, raw):
-                if self.get_config().get('anchor-type') == "random":
+                anchor_type = self.get_config().get('anchor-type')
+                if anchor_type == "random":
                     anchor = uuid.uuid4().hex[:6]
+                elif anchor_type == "custom":
+                    # Extract custom anchor from origin header.
+                    # The header should match the syntax:
+                    # `text (#anchor)`,
+                    # if matched, text and anchor get the value respectively,
+                    # otherwise fallback to slugify function.
+                    #
+                    # Firstly, this syntax give the user the ability to
+                    # customize the TOC anchor link.
+                    # `# How Lektor Works (#anchor-text)` renders to
+                    # <h1 id="anchor-text">How Lektor Works</h1>
+                    #
+                    # However this is a pretty workaround and useful way for
+                    # those non-ascii languages, such as CJK to use the
+                    # custom anchor as the slug.
+                    # `# 中文标题 (#chinese-title)` renders to
+                    # <h1 id="chinese-title">中文标题</h1>
+                    match = re.search(r'(.*)\(#(.*)\)', text)
+                    if match:
+                        text, anchor = match.groups()
+                        anchor = anchor.replace(' ', '')
+                    else:
+                        anchor = slugify(raw)
                 else:
                     anchor = slugify(raw)
                 renderer.meta['toc'].append((level, anchor, Markup(text)))
                 return '<h%d id="%s">%s</h%d>' % (level, anchor, text, level)
+
         config.renderer_mixins.append(HeaderAnchorMixin)
 
     def on_markdown_meta_init(self, meta, **extra):
