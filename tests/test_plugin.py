2edc14e6d2cf36cb8034a4f965078a90e2dd446f
Jeff Dairiki <dairiki@dairiki.org>
2023-03-03 17:46:18 -0800

    test: add basic tests

---
+++ tests/test_plugin.py
@@ -0,0 +1,80 @@
+import inspect
+import re
+from pathlib import Path
+
+from lektor.context import Context
+from lektor.metaformat import serialize
+from lektor.project import Project
+from markupsafe import Markup
+import pytest
+
+from lektor_markdown_header_anchors import MarkdownHeaderAnchorsPlugin
+
+
+def write_file(path, contents):
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(contents)
+
+
+@pytest.fixture
+def env(tmp_path):
+    project_dir = tmp_path
+    project_file = project_dir / "test.lektorproject"
+    write_file(project_file, "")
+    write_file(
+        project_dir / "models/page.ini",
+        (
+            "[fields.body]\n"
+            "type = markdown\n"
+        )
+    )
+
+    project = Project.from_file(project_file)
+    return project.make_env()
+
+
+@pytest.fixture
+def pad(env):
+    pad = env.new_pad()
+    with Context(pad=pad):
+        yield pad
+
+
+@pytest.fixture
+def make_markdown(pad):
+    def make_markdown(markdown_src):
+        fields = [("body", markdown_src)]
+        write_file(
+            Path(pad.env.root_path, "content/contents.lr"),
+            "".join(serialize(fields))
+        )
+        return pad.root["body"]
+
+    return make_markdown
+
+
+def test_html(make_markdown):
+    markdown = make_markdown("# This & That\n")
+    assert re.match(
+        r"<h1 id=([\"'])this-that\1>This &amp; That</h1>\s*\Z",
+        markdown.html
+    )
+
+
+def test_toc(make_markdown):
+    markdown = make_markdown(inspect.cleandoc(
+        """
+        ## Heading One
+        ## Heading Two
+        ### Heading 2.1
+        ## Heading Three
+        """
+    ))
+    toc = markdown.meta["toc"]
+    assert toc == [
+        ('heading-one', Markup('Heading One'), []),
+        ('heading-two', Markup('Heading Two'), [
+            ('heading-2.1', Markup('Heading 2.1'), []),
+        ]),
+        ('heading-three', Markup('Heading Three'), []),
+    ]
