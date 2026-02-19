5ceb069d6cc5f28c588f3500ffad10ddbef3e257
Jeff Dairiki <dairiki@dairiki.org>
2026-02-18 22:28:16 -0800

    fix: do not include markup in anchors

--- lektor_markdown_header_anchors.py
+++ lektor_markdown_header_anchors.py
@@ -1,9 +1,11 @@
 import uuid
 from collections import namedtuple
+from html.parser import HTMLParser
 from importlib import metadata
 
 import mistune
 from lektor.pluginsystem import Plugin
+from markupsafe import escape
 from markupsafe import Markup
 from slugify import slugify
 
@@ -19,8 +21,9 @@ class MarkdownHeaderAnchorsPlugin(Plugin):
         return dist.metadata.get("summary")
 
     def on_markdown_config(self, config, **extra):
+        anchor_type = self.get_config().get('anchor-type')
         config.renderer_mixins.append(
-            renderer_mixin_factory(slugify=self._slugify)
+            renderer_mixin_factory(anchor_type)
         )
 
     def on_markdown_meta_init(self, meta, **extra):
@@ -48,22 +51,27 @@ class MarkdownHeaderAnchorsPlugin(Plugin):
 
         meta['toc'] = toc
 
-    def _slugify(self, text):
-        if self.get_config().get('anchor-type') == "random":
-            return uuid.uuid4().hex[:6]
-        # NB: slugify decodes HTML entities in its input before slugifying
-        return slugify(text)
 
-
-def renderer_mixin_factory(slugify=slugify):
-    def render_header(text, level, meta):
-        anchor = slugify(text)
+def renderer_mixin_factory(anchor_type=None):
+    def render_header(text, level, meta, raw=None):
+        if anchor_type == "random":
+            anchor = uuid.uuid4().hex[:6]
+        else:
+            if raw is None:
+                # Mistune2 does not make the raw un-marked-up text available,
+                # so we have to regenerate it here by stripping HTML tags from
+                # the markup.
+                #
+                # NB: slugify() decodes HTML entities, as does _strip_tags().
+                # We need to encode here again to prevent double-decoding.
+                raw = escape(_strip_tags(text))
+            anchor = slugify(raw)
         meta['toc'].append((level, anchor, Markup(text)))
         return '<h%d id="%s">%s</h%d>' % (level, anchor, text, level)
 
     class RendererMixin_mistune0(object):
         def header(self, text, level, raw):
-            return render_header(text, level, self.meta)
+            return render_header(text, level, self.meta, raw)
 
     class RendererMixin_mistune2(object):
         def heading(self, text, level):
@@ -76,3 +84,31 @@ def renderer_mixin_factory(slugify=slugify):
         return RendererMixin_mistune2
     else:
         raise RuntimeError("unsupported mistune version")
+
+
+def _strip_tags(inline_html: str) -> str:
+    """Strip HTML tags from inline content.
+
+    This is a fairly stupid HTML tag stripper.  All tags are assumed to be inline.
+
+    NB: This should not be used for security/sanitization purposes. (E.g.
+    the bodies of <script> tags are not stripped.)
+    """
+    parser = _StripTagsParser()
+    parser.feed(inline_html)
+    return parser.close()
+
+
+class _StripTagsParser(HTMLParser):
+    _data: list[str]
+
+    def reset(self) -> None:
+        super().reset()
+        self._data = []
+
+    def handle_data(self, data: str) -> None:
+        self._data.append(data)
+
+    def close(self) -> str:
+        super().close()
+        return "".join(self._data)
